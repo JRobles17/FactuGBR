63d9ef17e5bd353a6052bce4918a6d6f9f061b3f
JRobles17 <roblesretamozoqwe@gmail.com>
2025-04-07 17:01:15 -0500

    first commit

---
+++ FactuGBR/src/main/java/com/mycompany/factugbr/FactuGBR.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.factugbr;
+
+/**
+ *
+ * @author ALUMNO
+ */
+public class FactuGBR {
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
